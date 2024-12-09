19595b52724518815acaa65708fa5ac0a45ef3ed
Evelyn Eleoterio <evelyneleoterio1997@gmail.com>
2024-12-08 22:08:24 -0300

    file main

--- api/main.go
+++ api/main.go
@@ -15,6 +15,6 @@ func main() {
 	r := mux.NewRouter()
 	routes.RegisterUserRoutes(r)
 
-	log.Println("API rodando na porta 8080...")
+	log.Println("API running on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
