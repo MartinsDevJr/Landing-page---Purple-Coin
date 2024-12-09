e6138ce366276c26c8cd461edcda756d72650272
Evelyn Eleoterio <evelyneleoterio1997@gmail.com>
2024-12-08 21:57:39 -0300

    create api and tested

--- api/main.go
+++ api/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/MartinsDevJr/purple_coin/routes"
+	"github.com/gorilla/mux"
+
+	"github.com/MartinsDevJr/purple_coin/db"
+)
 
 func main() {
+	db.InitDB()
+	r := mux.NewRouter()
+	routes.RegisterUserRoutes(r)
+
+	log.Println("API rodando na porta 8080...")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
